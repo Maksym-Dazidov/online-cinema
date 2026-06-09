37499fa04bc35357f2bad4796b612402c8705bbd
Maksym Dazidov <maksymdazidov@gmail.com>
2026-06-09 23:32:20 +0200

    feat(db): initialize async database connection and session setup

---
+++ app/db/session.py
@@ -0,0 +1,27 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+from app.core.config import settings
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=False,
+    future=True,
+)
+
+
+async_session_maker = sessionmaker( # type: ignore
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+async def get_db() -> AsyncSession:
+    async with async_session_maker() as session:
+        yield session
