eed5bd1623280cd5f3d70fc9f8f8ff0d570d88fe
Maksym Dazidov <maksymdazidov@gmail.com>
2026-06-14 23:42:04 +0200

    feat(roles): add lifespan for default roles initialization and register admin router

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 from app.api.auth import router as auth_router
 from app.api.users import router as users_router
 from app.api.actor import router as actor_router
 from app.api.genre import router as genre_router
 from app.api.movie import router as movie_router
+from app.api.admin import router as admin_router
+from app.db.session import async_session_maker
+from app.crud.user_group import user_group_crud
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with async_session_maker() as db:
+        await user_group_crud.create_default_groups(db)
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(auth_router)
 app.include_router(users_router)
 app.include_router(actor_router)
 app.include_router(genre_router)
 app.include_router(movie_router)
+app.include_router(admin_router)
 
 
 @app.get("/")
